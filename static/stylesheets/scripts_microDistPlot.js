36a87a0b339c9229e015e7076ed94c6ff474771b
nickp <i.nickpope@gmail.com>
2014-09-10 10:02:14 -0400

    Added some filters for getting class names to /filters/templatetags/..

---
+++ static/stylesheets/scripts_microDistPlot.js
@@ -0,0 +1,6 @@
+//Testing jqplot functionality for ultimately making
+//microspecies distribution plots
+
+$(document).ready(function(){
+  var plot1 = $.jqplot ('chart1', [[3,7,9,1,4,6,8,2,5]]); //Make simple line graph
+});
