d10f658b9bc1bcf1c7a6253d8ff93d138bfec21d
nickp <i.nickpope@gmail.com>
2014-10-29 08:06:43 -0400

    (minor change) - passing from laptop to office machine

--- models/gentrans/gentrans_model.py
+++ models/gentrans/gentrans_model.py
@@ -38,6 +38,13 @@ class gentrans(object):
 		logging.warning("GEN LIMIT: " + self.gen_limit)
 		logging.warning("CHEM: " + self.chem_struct)
 
+
+
+		#########################################
+		self.gen_limit = 3
+		#######################################
+
+
 		dataDict = {
 					'structure': self.chem_struct,
 					'generationLimit': self.gen_limit,
@@ -87,8 +94,9 @@ class gentrans(object):
 
 
 		# fileout = open('C:\\Documents and Settings\\npope\\Desktop\\out.txt', 'w')
-		# fileout.write(results.content)
-		# fileout.close()
+		fileout = open('C:\\Users\\nickpope\\Desktop\\out.txt', 'w')
+		fileout.write(results.content)
+		fileout.close()
 
 		# new_result = ''
 
@@ -115,103 +123,26 @@ class gentrans(object):
 		# 	setattr(self, key, value)
 
 
-"""
-This is a "hacked" way to transform the keys
-in the resultant json string into the ones
-used by jit.js. It just reads through the json
-string and replaces certain keys (e.g., smiles)
-with the jit.js keys (e.g., name).
-This bypasses any need of recursive statements (for now)
-"""
 def parseJsonForJit(jsonStr):
-	# index = jsonStr.find("smiles")
-	jsonStr = jsonStr.replace("smiles", "name")
-	# logging.warning(jsonStr)
-
-	fileout = open('C:\\Documents and Settings\\npope\\Desktop\\out.txt', 'w')
-	fileout.write(jsonStr)
-	fileout.close()
-
-	return jsonStr
-
-
-# def insert_dash(string, index):
-#     return string[:index] + '"name": ' + string[index:]
-
-
-def parseJsonForJit1(jsonStr, genPop):
-	"""
-	Need to reformat json to:
-	{
-		id: "id"
-		name: "name"
-		data: {}
-		children: [{}]
-	}
-	where children continues the tree
-	"""
-
-	logging.warning("GEN POP " + str(genPop))
 
 	jsonDict = json.loads(jsonStr)
 
 	root = jsonDict['results']
-	parent = root.keys()[0] # parent species
-	mainList = [parent] 
+	parent = root.keys()[0]
 
-	jitDict = {
-		"id": parent,
-		"name": parent,
-		"data": {},
-		"children": []
-	}
-
-	n = int(genPop) # number of generations of metabolites
-	relRoot = root[parent]['metabolites'] # initialize relative root
-	y = len(relRoot) # number of 1st gen metabolites
-	logging.warning(y)
-	recursion_loop(len(relRoot), n, mainList, relRoot)
-
-	# loop through each generation of metabolites
-	# for i in range(n):
-
-	# 	genList = [] # list per generation (to append to mainList)
-
-	# 	# loop through metabolites in generation
-	# 	for key in relRoot.keys():
-	# 		logging.warning("META KEY " + key)
-	# 		genList.append[key]
-
-	# 	mainList.append(genList)
-	# 	# still have to "walk" through dict
-
-	# logging.warning(str(jitDict))
-
-	# fileout = open('C:\\Documents and Settings\\npope\\Desktop\\out.txt', 'w')
-	# fileout.write(json.dumps(jitDict))
-	# fileout.close()
-
-	return None
+	recurse(root)
 
 
-"""
-Recursive loop for metabolites
-y - number of metabolites per generation
-n - number of generations
-mainList - list to append SMILES to (e.g., [parent, gen1List, gen2List, ...])
-"""
-def recursion_loop(y, n, mainList, relRoot):
+	return jsonStr
 
-	if n >= 1:
-		logging.warning("above loop")
-		for i in range(y):
-			genList = []
-			genList.append(relRoot.keys()[i]) # append key (SMILES) to genList
-			logging.warning("in loop: " + str(genList))
-			# relRoot = relRoot[key]['metabolites']
-			recursion_loop(y, n - 1, genList, relRoot)
-	# else:
-	# 	# whatever()
-	# 	stuff = 1
-	return None
+def recurse(root):
 
+	logging.warning("above loop")
+	#foreach node 
+	for key in root.keys():
+		logging.warning(key)
+		if key in root:
+			root = root[key]['metabolites']
+			recurse(root)
+		else:
+			logging.warning("key " + str(key) + " not in dict")
