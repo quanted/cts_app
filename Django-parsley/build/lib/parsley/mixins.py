3f08fb928776df0bb944db1b1fdbe9add9fef01e
popenc <i.nickpope@gmail.com>
2015-09-09 14:37:16 -0400

    no more django-parsley inside ubertool_cts root; changed manage.py back to recognize local_path

--- Django-parsley/build/lib/parsley/mixins.py
+++
@@ -1,8 +0,0 @@
-from parsley.decorators import parsleyfy
-
-
-class ParsleyAdminMixin(object):
-
-    def get_form(self, *args, **kwargs):
-        form = super(ParsleyAdminMixin, self).get_form(*args, **kwargs)
-        return parsleyfy(form)
