17fee1f38b3399da79d97139dda52a61ec62b5df
popenc <i.nickpope@gmail.com>
2015-09-09 12:44:54 -0400

    changed manage.py back to original

--- manage.py
+++ manage.py
@@ -4,18 +4,8 @@ import logging
 
 if __name__ == "__main__":
 
-	hasFile = False
-
-	try:
-		import local_path
-		hasFile = True
-	except ImportError:
-		pass
-
 	if os.path.abspath(__file__) == os.path.join('/', 'var', 'www', 'ubertool', 'ubertool_cts', 'manage.py'):
 		os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings_apache")
-	elif hasFile and os.path.abspath(__file__) == local_path.localPath():
-		os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings_local")
 	else:
 		os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
