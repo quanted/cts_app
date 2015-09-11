957236f1142861b8c2c8c58a82a6ccd55a3780d6
popenc <i.nickpope@gmail.com>
2015-09-11 11:17:45 -0400

    added efs filter SMILES function to smilesfilter.py

--- REST/smilesfilter.py
+++ REST/smilesfilter.py
@@ -55,21 +55,39 @@ def is_valid_smiles(smiles):
         return True
 
 
-def dehydrogenizeSMILES(smiles):
+def filterSMILES(smiles):
     """
     calls jchem web services through jchem_rest 
     """
+    
     request = requests.Request(data={'smiles': smiles}) # wrap smiles in http request
-    response = jchem_rest.removeExplicitHFromSMILES(request)
+    response = jchem_rest.filterSMILES(request)
     logging.info("RESPONSE: {}".format(response.content))
+
     try:
-        # dehydrogenized_smiles = response.json.get('structure')
-        dehydrogenizeSMILES = json.loads(response.content)['structure']
-        logging.info("NEW SMILES: {}".format(dehydrogenizeSMILES))
-        return dehydrogenizeSMILES
+        filtered_smiles = json.loads(response.content)['result'][0] # picks out smiles from efs???
+        logging.info("NEW SMILES: {}".format(filtered_smiles))
+        return filtered_smiles
     except Exception as e:
         logging.info("> dehydrogenize in smilesfilter error: {}".format(e))
         raise e
+
+
+# def dehydrogenizeSMILES(smiles):
+#     """
+#     calls jchem web services through jchem_rest 
+#     """
+#     request = requests.Request(data={'smiles': smiles}) # wrap smiles in http request
+#     response = jchem_rest.removeExplicitHFromSMILES(request)
+#     logging.info("RESPONSE: {}".format(response.content))
+#     try:
+#         # dehydrogenized_smiles = response.json.get('structure')
+#         dehydrogenizeSMILES = json.loads(response.content)['structure']
+#         logging.info("NEW SMILES: {}".format(dehydrogenizeSMILES))
+#         return dehydrogenizeSMILES
+#     except Exception as e:
+#         logging.info("> dehydrogenize in smilesfilter error: {}".format(e))
+#         raise e
 
 
 # def transformSMILES(smiles):
@@ -82,33 +100,33 @@ def dehydrogenizeSMILES(smiles):
 
 
 
-def getDominateTautomer(smiles):
-    """
-    calls jchem_rest tautomer code, returns smiles
-    of dominant tautomer from jchem ws response
-    """
+# def getDominateTautomer(smiles):
+#     """
+#     calls jchem_rest tautomer code, returns smiles
+#     of dominant tautomer from jchem ws response
+#     """
 
-    taut_obj = JProp.getPropObject('tautomerization')
-    # taut_obj.setPostDataValues({
-        # "maxStructureCount": self.tautomer_maxNoOfStructures,
-        # "pH": self.tautomer_pH,
-        # "considerPH": True
-    # })
-    taut_obj.makeDataRequest(smiles)
-    logging.info("taut results: {}".format(taut_obj.results))
+#     taut_obj = JProp.getPropObject('tautomerization')
+#     # taut_obj.setPostDataValues({
+#         # "maxStructureCount": self.tautomer_maxNoOfStructures,
+#         # "pH": self.tautomer_pH,
+#         # "considerPH": True
+#     # })
+#     taut_obj.makeDataRequest(smiles)
+#     logging.info("taut results: {}".format(taut_obj.results))
 
-    try:
-        dominant_taut = taut_obj.results['result'][0]['structureData']['structure']
-        logging.info("dominant taut: {}".format(dominant_taut))
+#     try:
+#         dominant_taut = taut_obj.results['result'][0]['structureData']['structure']
+#         logging.info("dominant taut: {}".format(dominant_taut))
 
-        request = requests.Request(data={'chemical': dominant_taut})
-        response = jchem_rest.mrvToSmiles(request)
-        logging.info("RESPONSE: {}".format(response.content))
+#         request = requests.Request(data={'chemical': dominant_taut})
+#         response = jchem_rest.mrvToSmiles(request)
+#         logging.info("RESPONSE: {}".format(response.content))
 
-        dominant_taut_smiles = json.loads(response.content)['structure']
-        return dominant_taut_smiles
+#         dominant_taut_smiles = json.loads(response.content)['structure']
+#         return dominant_taut_smiles
 
-    except Exception as e:
-        logging.info("error at getDominateTautomer: {}".format(e))
-        raise e
-    # jchemDataDict.update({taut_obj.name: taut_obj.results})
+#     except Exception as e:
+#         logging.info("error at getDominateTautomer: {}".format(e))
+#         raise e
+#     # jchemDataDict.update({taut_obj.name: taut_obj.results})
