e7c52a976795586011b8909c649c81f68589500b
kurtw555 <kurt_wolfe@yahoo.com>
2015-11-18 16:13:37 -0500

    Added 'CTS_EFS_SERVER' key to seetings_apache.py

--- settings_apache.py
+++ settings_apache.py
@@ -29,6 +29,7 @@ os.environ.update({
     'CTS_JCHEM_SERVER': 'http://172.20.100.12',       # jchem rest server (internal)
     # 'CTS_EPI_SERVER': 'http://172.20.100.16',         # test rest rserver (internal)
     'CTS_EPI_SERVER': 'http://win.pg.gl',
+    'CTS_EFS_SERVER': 'http://134.67.114.2',
     'CTS_SPARC_SERVER': 'http://204.46.160.69:8080/sparc-integration/rest/calc/multiProperty',         # SPARC rest server (external)
     'wkhtmltopdf': PROJECT_ROOT + '/wkhtmltopdf/linux/wkhtmltopdf',
     'PROJECT_PATH': PROJECT_ROOT,
