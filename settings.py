283881fca3009784c96059b89b31ccab9289256c
popenc <i.nickpope@gmail.com>
2015-12-15 11:07:58 -0500

    TEST pointing to 8080

--- settings.py
+++ settings.py
@@ -26,8 +26,8 @@ os.environ.update({
     # # 'UBERTOOL_REST_SERVER': 'http://localhost:80',
     # 'UBERTOOL_REST_SERVER': 'http://54.83.18.251:80',
     # 'CTS_TEST_SERVER': 'http://pg.gl', # test rest server (internal)
-    'CTS_TEST_SERVER': 'http://134.67.114.6',
-    'CTS_EPI_SERVER': 'http://134.67.114.6',
+    'CTS_TEST_SERVER': 'http://134.67.114.6:8080',
+    'CTS_EPI_SERVER': 'http://134.67.114.6:80',
     'CTS_JCHEM_SERVER': 'http://134.67.114.2',
     'CTS_EFS_SERVER': 'http://134.67.114.2',
     # 'CTS_JCHEM_SERVER': 'http://localhost:8080',
--- settings_apache.py
+++ settings_apache.py
@@ -28,9 +28,9 @@ os.environ.update({
     'UBERTOOL_MONGO_SERVER': 'http://uberrest-topknotmeadows.rhcloud.com',
     'UBERTOOL_SECURE_SERVER': 'http://uberrest-topknotmeadows.rhcloud.com',   
     'UBERTOOL_REST_SERVER': 'http://172.20.100.15:7777',        # CGI Internal
-    'CTS_TEST_SERVER': 'http://172.20.100.16',                   # test rest server (intranet)
+    'CTS_TEST_SERVER': 'http://172.20.100.16:8080',                   # test rest server (intranet)
     'CTS_JCHEM_SERVER': 'http://172.20.100.12',       # jchem rest server (internal)
-    'CTS_EPI_SERVER': 'http://172.20.100.16',
+    'CTS_EPI_SERVER': 'http://172.20.100.16:80',
     'CTS_EFS_SERVER': 'http://172.20.100.12',
     'CTS_SPARC_SERVER': 'http://204.46.160.69:8080/sparc-integration/rest/calc/multiProperty',         # SPARC rest server (external)
     'wkhtmltopdf': PROJECT_ROOT + '/wkhtmltopdf/linux/wkhtmltopdf',
