94019c2a547c036380cd2b3ea6dab2c648613998
popenc <i.nickpope@gmail.com>
2016-04-22 16:02:36 -0400

    separated celery instance and tasks code into two files

---
+++ celery_cts/celery.py
@@ -0,0 +1,31 @@
+"""
+CTS celery instance
+"""
+
+import os
+from celery import Celery
+import logging
+import importlib
+import json
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings_apache')
+
+from django.conf import settings
+
+# app = Celery('celery', broker='redis://localhost:6379/0', backend='redis://localhost:6379/0')
+app = Celery(broker='redis://localhost:6379/0')
+
+import celery_config  # import it first?
+# app.config_from_object('celery_config')
+
+# Using a string here means the worker will not have to
+# pickle the object when using Windows.
+app.config_from_object('django.conf:settings')
+# app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+logging.getLogger('celery.task.default').setLevel(logging.DEBUG)
+logging.getLogger().setLevel(logging.DEBUG)
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
