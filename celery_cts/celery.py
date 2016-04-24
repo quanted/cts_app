1ee2bf1921cd716c723b9d13faa7bc2f56b8eb29
popenc <i.nickpope@gmail.com>
2016-04-24 15:41:17 -0400

    moving code to vm for testing celery daemons

--- celery_cts/celery.py
+++ celery_cts/celery.py
@@ -21,19 +21,23 @@ app.conf.update(
     CELERY_ACCEPT_CONTENT = ['json'],
 	CELERY_TASK_SERIALIZER = 'json',
 	CELERY_RESULT_SERIALIZER = 'json',
-	CELERYD_NODES=6,
+	# CELERYD_NODES=6,
+	CELERY_NODES="test_worker chemaxon_worker epi_worker sparc_worker measured_worker manager_worker",
 	CELERY_BIN="/var/www/ubertool/virtualenv/bin/celery",
 	CELERY_APP="tasks:app",
 	CELERYD_CHDIR="/var/www/ubertool/ubertool_cts",
-	CELERYD_OPTS="-Q:1 test -c:1 1 -n:1 test_worker \
-		-Q:2 chemaxon -c:2 8 -n:2 chemaxon_worker \
-		-Q:3 epi -c:3 8 -n:3 epi_worker \
-		-Q:4 sparc -c:4 1 -n:4 sparc_worker \
-		-Q:5 measured -c:5 1 -n:5 measured_worker \
-		-Q:6 manager -n:6 manager_worker",
+	CELERYD_OPTS="-Q:1 test -c:1 1 \
+		-Q:2 chemaxon -c:2 8 \
+		-Q:3 epi -c:3 8 \
+		-Q:4 sparc -c:4 1 \
+		-Q:5 measured -c:5 1 \
+		-Q:6 manager",
 	CELERYD_USER="celery",
 	CELERYD_GROUP="celery",
-	CELERY_CREATE_DIRS=1
+	CELERY_CREATE_DIRS=1,
+	# %N will be replaced with the first part of the nodename.
+	CELERYD_LOG_FILE="/var/log/celery/%N.log",
+	CELERYD_PID_FILE="/var/run/celery/%N.pid",
 )
 
 # import celery_config  # import it first?
