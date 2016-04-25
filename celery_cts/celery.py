dd2ddf4761e9a364c4bb4fbce2bb11021eb5761c
popenc <i.nickpope@gmail.com>
2016-04-24 20:39:28 -0400

    moving code to vm for testing celery daemons

--- celery_cts/celery.py
+++ celery_cts/celery.py
@@ -21,18 +21,18 @@ app.conf.update(
     CELERY_ACCEPT_CONTENT = ['json'],
 	CELERY_TASK_SERIALIZER = 'json',
 	CELERY_RESULT_SERIALIZER = 'json',
-	CELERYD_NODES=6,
-	CELERY_BIN="/var/www/ubertool/virtualenv/bin/celery",
-	CELERY_APP="tasks:app",
-	CELERYD_CHDIR="/var/www/ubertool/ubertool_cts",
-	CELERYD_USER="celery",
-	CELERYD_GROUP="celery",
+	# CELERYD_NODES=6,
+	# CELERY_BIN="/var/www/ubertool/virtualenv/bin/celery",
+	# CELERY_APP="tasks:app",
+	# CELERYD_CHDIR="/var/www/ubertool/ubertool_cts",
+	# CELERYD_USER="celery",
+	# CELERYD_GROUP="celery",
 	CELERY_CREATE_DIRS=1,
 	# %N will be replaced with the first part of the nodename.
 	# CELERYD_LOG_FILE="/var/log/celery/%n.log",
 	# CELERYD_PID_FILE="/var/run/celery/%n.pid",
-	CELERYD_LOG_FILE="%n.log",
-	CELERYD_PID_FILE="%n.pid",
+	# CELERYD_LOG_FILE="%n.log",
+	# CELERYD_PID_FILE="%n.pid",
 )
 
 # import celery_config  # import it first?
