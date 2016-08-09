06c719dbb3dfa997ac3b41b8ea3001a44101b64a
popenc <i.nickpope@gmail.com>
2016-08-09 13:57:14 -0400

    (1.1.19) trying to add path argument to nodejs server and cts_pchemprop_ajax_calls

--- nodejs/node_server.js
+++ nodejs/node_server.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var server = http.createServer().listen(4000);
-var io = require('socket.io').listen(server);
+// var io = require('socket.io').listen(server);
+var io = require('socket.io').listen(server, {path: '/sockets'});
 
 var cookie_reader = require('cookie');
 var querystring = require('querystring');
