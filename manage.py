59141105e482d8c40116a2c6b3548ea481d5c229
popenc <i.nickpope@gmail.com>
2016-11-04 13:53:25 -0400

    changed celery workers batch file name to be technically correct; cleaned up manage

--- manage.py
+++ manage.py
@@ -4,14 +4,14 @@ import logging
 
 if __name__ == "__main__":
 
-    has_file = os.path.isfile("settings_local.py")
+    # has_file = os.path.isfile("settings_local.py")
 
-    logging.info(has_file)
+    # logging.info(has_file)
 
-    if has_file:
-        # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings_local")
-        os.environ['DJANGO_SETTINGS_MODULE'] = 'settings_local'
-    elif os.path.abspath(__file__) == os.path.join('/', 'var', 'www', 'ubertool', 'ubertool_cts', 'manage.py'):
+    # if has_file:
+    #     # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings_local")
+    #     os.environ['DJANGO_SETTINGS_MODULE'] = 'settings_local'
+    if os.path.abspath(__file__) == os.path.join('/', 'var', 'www', 'ubertool', 'ubertool_cts', 'manage.py'):
         # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings_apache")
         os.environ['DJANGO_SETTINGS_MODULE'] = 'settings_apache'
     else:
