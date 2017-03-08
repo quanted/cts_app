c0ea190a605e300d8de0cfe3ae5c935573e12796
popenc <i.nickpope@gmail.com>
2017-03-08 11:00:00 -0500

    removed cts_channels from cts_app level

--- __init__.py
+++ __init__.py
@@ -1,3 +1,9 @@
 # from __future__ import absolute_import
 # # # from .celery import app as celery_app
 # from .tasks import app as celery_app
+# import logging
+# import cts_calcs
+# from cts_app import cts_calcs
+# from cts_calcs import chemaxon_cts
+# from cts_calcs.chemaxon_cts import worker
+# logging.warning("__init__ at cts_app dir: {}".format(dir(chemaxon_cts)))
