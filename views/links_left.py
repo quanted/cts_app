bd6bf2e09b44488f05277e90e215ab34f7984abf
popenc <i.nickpope@gmail.com>
2018-07-19 17:16:15 -0400

    updated link to newest user's guide

--- views/links_left.py
+++ views/links_left.py
@@ -25,7 +25,7 @@ def ordered_list(model=None, page=None):
         ),
         ('Documentation', OrderedDict([
                 # ('Download CTS User Guide (PDF)', 'static_qed/cts/docs/CTS_USER_Guide_5-8-17.pdf'),
-                ('Download CTS User Guide (PDF)', 'static_qed/cts/docs/CTS_USER_Guide_7-18-2018.pdf'),
+                ('Download CTS User Guide (PDF)', 'static_qed/cts/docs/CTS_USER_Guide_7-19-2018.pdf'),
                 ('API Documentation', 'cts/rest/'),
                 ('Manuscripts', 'cts/about/manuscripts'),
                 # ('Source Code', 'https://github.com/quanted/cts_app'),
