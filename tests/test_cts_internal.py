6accf5e399b85c0aeda985e9d9992c4867fd7db9
popenc <i.nickpope@gmail.com>
2018-08-06 16:40:21 -0400

    cts internal links tests added; updated cts_calcs ref

--- tests/test_cts_internal.py
+++ tests/test_cts_internal.py
@@ -1,32 +1,38 @@
+import sys
 import requests
 import unittest
 import numpy.testing as npt
-import linkcheck_helper
+# import linkcheck_helper
+import cts_app.tests.linkcheck_helper as linkcheck_helper
+from cts_app.tests.test_objects import get_post_object
+from django.test import Client, TestCase
 
 test = {}
 
-servers = ["https://qedinternal.epa.gov/cts/", "http://127.0.0.1:8000/cts/"]
-# servers = ["http://127.0.0.1:8000/cts/"]
+# servers = ["https://qedinternal.epa.gov/cts/", "http://127.0.0.1:8000/cts/"]
+servers = ["http://127.0.0.1:8000/cts/"]
 
-pages = {
-    'GET': ["", "rest/", "chemspec", "pchemprop", "gentrans", "chemspec/input", "pchemprop/input",
-            "gentrans/input", "chemspec/batch", "pchemprop/batch", "gentrans/batch"],
-    'POST': []
-}
+pages = ["", "rest/", "chemspec", "pchemprop", "gentrans",
+        "chemspec/input", "pchemprop/input", "gentrans/input",
+        "chemspec/batch", "pchemprop/batch", "gentrans/batch"]
+
+# output_pages = ["chemspec/output", "pchemprop/output", "gentrans/output"]  # require POST
+workflow_endpoints = ["/cts/chemspec/output"]  # require POST  (returned a CSRF 403..)
 
 # TODO: replace api_endpoints array with endpoints from cts
 api_endpoints = []
 
 # following are lists of url's to be processed with tests below
-# check_pages = [s + p for s in servers for p in pages]
-check_pages = []
-for key,val in pages.items():
-    check_pages += [s + p for s in servers for p in pages[key]]
+check_pages = [s + p for s in servers for p in pages]
 
-print("check pages: {}".format(check_pages))
+# workflow_test_urls = [s + p for s in servers for p in output_pages]
+
+print("checking pages: {}".format(check_pages))
+# print("checking workflow outputs at: {}".format(workflow_test_urls))
 
 
-class TestCTSPages(unittest.TestCase):
+# class TestCTSPages(unittest.TestCase):
+class TestCTSPages(TestCase):
     """
     this testing routine accepts a list of pages and performs a series of unit tests that ensure
     that the web pages are up and operational on the server.
@@ -56,6 +62,48 @@ class TestCTSPages(unittest.TestCase):
             print("Error '{0}' occured. Arguments {1}.".format(e.message, e.args))
         finally:
             linkcheck_helper.write_report(test_name, assert_error, check_pages, response)
+        return
+
+    @staticmethod
+    def test_cts_workflows():
+
+        test_client = Client()
+
+        test_name = "Check workflow outputs "
+        try:
+            assert_error = False
+            # response = [requests.get(p).status_code for p in check_pages]
+
+            response = []
+            for p in workflow_endpoints:
+                workflow = p.split('/')[0]  # assuming model/output url structure, grabbing "model" part
+
+                post_data = get_post_object(workflow)
+
+                print("workflow: {}".format(workflow))
+                print("url: {}".format(p))
+                print("post: {}".format(post_data))
+
+                res = test_client.post(p, get_post_object(workflow))
+
+
+                response.append(res.status_code)
+            try:
+
+                print(">>> Response: {}".format(response))
+
+                npt.assert_array_equal(response, 200, '200 error', True)
+            except AssertionError:
+                assert_error = True
+            except Exception as e:
+                # handle any other exception
+                print("Error '{0}' occured. Arguments {1}.".format(e.message, e.args))
+        except Exception as e:
+            # handle any other exception
+            print("Error '{0}' occured. Arguments {1}.".format(e.message, e.args))
+        finally:
+            print("response: {}".format(response))
+            linkcheck_helper.write_report(test_name, assert_error, workflow_endpoints, response)
         return
 
     @staticmethod
---
+++ tests/test_objects.py
@@ -0,0 +1,38 @@
+"""
+POST objects to test specific CTS workflows.
+"""
+
+chemspec_post = {
+	'chem_struct':'aspirin',
+	'get_pka':'on',
+	'pKa_decimals':'2',
+	'pKa_pH_lower':'0',
+	'pKa_pH_upper':'14',
+	'pKa_pH_increment':'0.2',
+	'pH_microspecies':'7.0',
+	'isoelectricPoint_pH_increment':'0.5',
+	'get_taut':'on',
+	'tautomer_maxNoOfStructures':'100',
+	'tautomer_pH':'7.0',
+	'get_stereo':'on',
+	'stereoisomers_maxNoOfStructures':'100',
+	'chemical':'aspirin',
+	'orig_smiles':'CC(=O)OC1=C(C=CC=C1)C(O)=O',
+	'smiles':'CC(=O)OC1=C(C=CC=C1)C(O)=O',
+	'preferredName':'Aspirin',
+	'iupac':'2-(acetyloxy)benzoic acid',
+	'formula':'C9H8O4',
+	'casrn':'50-78-2',
+	'cas':'50-78-2,11126-35-5,11126-37-7,2349-94-2,26914-13-6,98201-60-6',
+	'dtxsid':'DTXSID5020108',
+	'mass':'180.159',
+	'exactmass':'180.042258738'
+}
+
+def get_post_object(workflow):
+	"""
+	Returns example POST object for a given workflow.
+	"""
+
+	if workflow == 'chemspec':
+		return chemspec_post
