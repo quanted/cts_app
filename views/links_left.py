b8886f149aeaf51d5fce2e365137d0f8dfc1f8b9
popenc <i.nickpope@gmail.com>
2019-03-15 14:48:33 -0400

    moved about section to top

--- views/links_left.py
+++ views/links_left.py
@@ -1,22 +1,9 @@
 from django.template.loader import render_to_string
 from collections import OrderedDict
 
-
 # 03ubertext_links_left:
 def ordered_list(model=None, page=None):
     link_dict = OrderedDict([
-        ('Execute CTS Workflows', OrderedDict([
-                ('Calculate Chemical Speciation', 'cts/chemspec'),
-                ('Calculate Physicochemical Properties', 'cts/pchemprop'),
-                ('Generate Transformation Products', 'cts/gentrans'),
-            ])
-        ),
-        # ('About', OrderedDict([
-        #         ('Chemical Editor', 'cts/module/chemedit-description'),
-        #         ('Physicochemical Properties', 'cts/module/pchemprop-description'),
-        #         ('Reaction Pathway Simulator', 'cts/module/reactsim-description'),
-        #     ])
-        # ),
         ('About', OrderedDict([
                 ('CTS Modules', 'cts/about/modules'),
                 ('Physicochemical Calculators', 'cts/about/pchemcalcs'),
@@ -25,9 +12,13 @@ def ordered_list(model=None, page=None):
                 ('Contact', 'cts/about/contact'),
             ])
         ),
+        ('Execute CTS Workflows', OrderedDict([
+                ('Calculate Chemical Speciation', 'cts/chemspec'),
+                ('Calculate Physicochemical Properties', 'cts/pchemprop'),
+                ('Generate Transformation Products', 'cts/gentrans'),
+            ])
+        ),
         ('Documentation', OrderedDict([
-                # ('Download CTS User Guide (PDF)', 'static_qed/cts/docs/CTS_USER_Guide_5-8-17.pdf'),
-                # ("Download CTS User's Guide (PDF)", 'static_qed/cts/docs/CTS_USER_Guide_7-19-2018.pdf'),
                 ("Download CTS User's Guide (PDF)", 'static_qed/cts/docs/CTS_USER_Guide_10-03-2018.pdf'),
                 ('API Documentation', 'cts/rest/'),
                 ('Manuscripts', 'cts/about/manuscripts'),
@@ -36,23 +27,8 @@ def ordered_list(model=None, page=None):
                 ('CTS Errors', 'cts/about/errors'),
                 # ('Source Code', 'https://github.com/quanted/cts_app'),
             ])
-        ),
-        # ('')
-        # ('Access Databases', OrderedDict([
-        # 		('FIFRA Chemicals', 'fifra'),
-        # 		('Flame Retardants', 'flame'),
-        # 	])
-        # ),
-        # ('Reaction Library Databases', OrderedDict([
-        # 		('Abiotic Hydrolysis', 'ahydrolysis'),
-        # 		('Abiotic Reduction', 'areduction'),
-        # 		('Mammalian Metabolism', 'mammet'),
-        # 	])
-        # ),
+        )
     ])
-
-    # html = render_to_string('03cts_ubertext_links_left.html', {'link_dict': link_dict})
-    # return html
 
     return render_to_string('03ubertext_links_left_drupal_cts.html', {
         'LINK_DICT': link_dict,
