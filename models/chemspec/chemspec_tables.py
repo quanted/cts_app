50a625e920bb9b81ef91bda1b5ba778bc73fcaa0
popenc <i.nickpope@gmail.com>
2019-07-22 12:49:44 -0400

    moved qtip js and css references to the cts drupal scripts and css template files

--- models/chemspec/chemspec_tables.py
+++ models/chemspec/chemspec_tables.py
@@ -108,10 +108,10 @@ inTmpl = Template(getInputTemplate())
 
 
 def table_all(chemspec_obj):
-    html_all = '<script type="text/javascript" src="/static_qed/cts/js/qtip/jquery.qtip.js"></script>'
-    html_all += '<link type="text/css" rel="stylesheet" href="/static_qed/cts/js/qtip/jquery.qtip.css"></link>'
+    # html_all = '<script type="text/javascript" src="/static_qed/cts/js/qtip/jquery.qtip.js"></script>'
+    # html_all += '<link type="text/css" rel="stylesheet" href="/static_qed/cts/js/qtip/jquery.qtip.css"></link>'
 
-    html_all += render_to_string('cts_downloads.html', {'run_data': mark_safe(json.dumps(chemspec_obj.run_data))})
+    html_all = render_to_string('cts_downloads.html', {'run_data': mark_safe(json.dumps(chemspec_obj.run_data))})
     html_all += table_inputs(chemspec_obj)
     html_all += table_outputs(chemspec_obj)
     # qtip popup script for images with class "wrapped_molecule"
