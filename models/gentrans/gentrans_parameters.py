08da5c807221f8575c60fe35c33ff016e0fc7ff7
popenc <i.nickpope@gmail.com>
2020-06-11 18:07:00 -0400

    added environment name for scripts_gentrans.js; split photolysis into ranked and unranked

--- models/gentrans/gentrans_parameters.py
+++ models/gentrans/gentrans_parameters.py
@@ -239,8 +239,9 @@ class cts_respiration(forms.Form):
 @parsleyfy
 class cts_reaction_libs(forms.Form):
 	abiotic_hydrolysis = forms.BooleanField(required=False, label='Abiotic Hydrolysis')
-	abiotic_reduction = forms.BooleanField(required=False, label='Abiotic Reduction')
-	photolysis = forms.BooleanField(required=False, label='Photolysis')
+	abiotic_reduction = forms.BooleanField(required=False, label='Abiotic Reduction', disabled=True)
+	photolysis = forms.BooleanField(required=False, label='Direct Photolysis (unranked)')  # unranked direct photolysis
+	ranked_photolysis = forms.BooleanField(required=False, label='Direct Photolysis (ranked) (under development)')  # ranked direct photolysis
 	aerobic_biodegrad = forms.BooleanField(required=False, label='Aerobic Biodegradation (under development)')
 	anaerobic_biodegrad = forms.BooleanField(required=False, label='Anaerobic Biodegradation (under development)')
 	mamm_metabolism = forms.BooleanField(required=False, label='Human Phase 1 Metabolism')
