48da5e82d209f2cc405736c2775fb0c6c1555a8c
popenc <i.nickpope@gmail.com>
2021-03-30 10:16:07 -0400

    update for displaying server migration on cdx

--- views/landing.py
+++ views/landing.py
@@ -3,35 +3,32 @@ from django.http import HttpResponse
 import importlib
 from .links_left import ordered_list
 import os
+import logging
+import bleach
 
 
 def ctsLandingPage(request):
-    # text_file2 = open(os.path.join(os.environ['PROJECT_PATH'], 'cts_app/views/main_text.txt'),'r')
-    # xx = text_file2.read()
 
-    #drupal template for header with bluestripe
-    #html = render_to_string('01epa_drupal_header.html', {})
     html = render_to_string('01epa_drupal_header.html', {
         'SITE_SKIN': os.environ['SITE_SKIN'],
         'title': "CTS"
     })
-    #html = render_to_string('01uberheader_main_drupal.html', {
-    #    'SITE_SKIN': os.environ['SITE_SKIN'],
-    #    'TITLE': u"\u00FCbertool"
-    #})
+
     html += render_to_string('02epa_drupal_header_bluestripe_onesidebar.html', {})
-    html += render_to_string('03epa_drupal_section_title_cts.html', {})
 
-    #main body of text
-    #html += render_to_string('04uber_drupal_frog_intro.html', {})
-    #http://jsfiddle.net/9zGQ8/
+    if os.getenv("ENV_NAME") == 'cgi_azure_docker_dev':
+        html += render_to_string('03epa_drupal_section_title_cts.html',
+            {
+                "message": bleach.clean('<b>NOTICE:</b> CTS is moving to \
+                    <a href="https://qed.epa.gov/cts">qed.epa.gov/cts</a> in the near future.<br>')
+            }
+        )
+    else:
+        html += render_to_string('03epa_drupal_section_title_cts.html', {"message": ""})
 
-    # fix attempt for &beta; in .txt encoding issue when pushing/pulling on github
     main_text_html = render_to_string('cts_landing_text.html', {})
     html += render_to_string('06cts_ubertext_start_index_drupal.html', {
-        # 'TITLE': 'Chemical Transformation Simulator',
         'TEXT_PARAGRAPH': main_text_html
-        # 'TEXT_PARAGRAPH': xx
     })
 
     html += render_to_string('07ubertext_end_drupal.html', {})
@@ -39,7 +36,6 @@ def ctsLandingPage(request):
 
     #scripts and footer
     html += render_to_string('09epa_drupal_ubertool_css.html', {})
-    #html += render_to_string('09epa_drupal_ubertool_scripts.html', {})
     html += render_to_string('09epa_drupal_cts_css.html')
     html += render_to_string('09epa_drupal_cts_scripts.html')
     html += render_to_string('10epa_drupal_footer.html', {})
