4b054dc12e04298afbf8539ba116ab44decd42c8
popenc <i.nickpope@gmail.com>
2023-02-07 15:32:55 -0500

    tabs for indentation

--- models/gentrans/gentrans_parameters.py
+++ models/gentrans/gentrans_parameters.py
@@ -211,7 +211,7 @@ mammalian_CHOICES = (('0', 'ChemAxon Metabolizer'), ('1', 'Biotransformer'))
 gen_limit_CHOICES = (('1', 1), ('2', 2), ('3', 3), ('4', 4))  # generation limit
 gen_limit_max = gen_limit_CHOICES[-1][1]  # not used as field, but referenced in many-a-place
 pop_limit_CHOICES = (('0', '0'), ('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'),
-                     ('5', '5'), ('6', '6'), ('7', '7'), ('8', '8'))  # population limit
+					 ('5', '5'), ('6', '6'), ('7', '7'), ('8', '8'))  # population limit
 tree_type_CHOICES = (('full_tree', 'Full tree'), ('simplified_tree', 'Simplified tree'))
 
 # aerobic_CHOICES = (('0', 'Surface Water'), ('1', 'Surface Soil'), ('2', 'Vadose Zone'), ('3', 'Groundwater'))
@@ -374,8 +374,8 @@ class cts_reaction_options(forms.Form):
 	# 				)
 
 	# add clean to force select option range
-    # def clean_status(self):
-    #    return self.instance.status
+	# def clean_status(self):
+	#    return self.instance.status
 
 
 # OECD Guidelines Selection
