fa1dc9d3255dcb1e6a52576eef088b4b37ab9d60
popenc <i.nickpope@gmail.com>
2026-02-06 09:08:53 -0500

    updated cts_calcs

--- models/generate_timestamp.py
+++ models/generate_timestamp.py
@@ -2,7 +2,7 @@ from datetime import datetime, timezone
 from zoneinfo import ZoneInfo
 
 def gen_jid(self):
-	ts = datetime.now(timezone.UTC)
-	localDatetime = ts.astimezone(ZoneInfo.timezone('US/Eastern'))
+	ts = datetime.now(timezone.utc)
+	localDatetime = ts.astimezone(ZoneInfo('US/Eastern'))
 	jid = localDatetime.strftime('%Y%m%d%H%M%S%f')
 	return jid
