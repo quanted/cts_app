bee9439cc1cf7a3a55d9f6c9a88d379ffa58749f
popenc <i.nickpope@gmail.com>
2026-02-09 12:28:27 -0500

    added pytz back

--- models/generate_timestamp.py
+++ models/generate_timestamp.py
@@ -1,8 +1,8 @@
-from datetime import datetime, timezone
+from datetime import datetime
 import pytz
 
 def gen_jid(self):
 	ts = datetime.now(pytz.UTC)
-	localDatetime = ts.astimezone(timezone('US/Eastern'))
+	localDatetime = ts.astimezone(pytz.timezone('US/Eastern'))
 	jid = localDatetime.strftime('%Y%m%d%H%M%S%f')
 	return jid
