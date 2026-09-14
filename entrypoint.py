da8dad332cd0480ce9c0135ac0d315e4bb4785d4
popenc <i.nickpope@gmail.com>
2026-09-14 13:26:24 -0400

    minor updates to entrypoint.py

--- entrypoint.py
+++ entrypoint.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
+import logging
 import os
 import django
 from django.core.management import call_command
 
-# initialize the app registry — django-admin/manage.py do this for you,
-# but calling call_command() directly does not.
+logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
+log = logging.getLogger("entrypoint")
+
 django.setup()
 
+# log.info("entrypoint: running collectstatic")
+# call_command("collectstatic", "--noinput", "--clear")
+
+log.info("entrypoint: running migrations")
 call_command("migrate", "auth", "--noinput")
 call_command("migrate", "sessions", "--noinput")
 
-# hand off to uwsgi, replacing this process (PID 1)
+log.info("entrypoint: handing off to uwsgi")
 os.execvp("uwsgi", ["uwsgi", "--ini", "/etc/uwsgi/uwsgi.ini"])
